1c59de5c2d15beb60d96bd2ae34a4d2174be9a3c
sc0rbetz <scorbetz@mail.ru>
2022-09-15 18:07:51 +0300

    02--сделал модель User

---
+++ backend/models/models.js
@@ -0,0 +1,36 @@
+const sequelize = require('../db')
+const { DataTypes } = require('sequelize')
+const { FOREIGNKEYS } = require('sequelize/types/query-types')
+
+const User = sequelize.define('user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    firstname: { type: DataTypes.STRING },
+    secondname: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    phonenumber: { type: DataTypes.STRING, unique: true },
+    dateofregistration: { type: DataTypes.TIME },
+    adcount: { type: DataTypes.INTEGER },
+    countryid: { type: DataTypes.STRING },
+    roleid: { type: DataTypes.STRING },
+    rating: { type: DateTypes.STRING },
+    rating: { type: DateTypes.DOUBLE }
+})
+
+//const ADinfo = sequelize.define('adinfo', {
+//    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+//    title: { type: DataTypes.STRING },
+//    description: {type: DataTypes.STRING}
+//})
+//const AD = sequelize.define('ad', {
+//    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+//    rating: { type: DataTypes.DOUBLE },
+//    price: { type: DataTypes.DOUBLE },
+//    img: { type: DataTypes.BLOB },
+//    infoid: { type: DateTypes.INTEGER, references: {
+
+//}}
+//})
+module.exports = {
+    User
+}
