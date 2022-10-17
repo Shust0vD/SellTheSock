22c194a928c9ad30e3642b89d21eaf0abd60198c
sc0rbetz <scorbetz@mail.ru>
2022-10-17 12:27:43 +0300

    02 - Пулл только models + исправление ошибок совместимости

--- backend/models/models.js
+++ backend/models/models.js
@@ -3,17 +3,73 @@ const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  username: { type: DataTypes.STRING, unique: true },
-  password: { type: DataTypes.STRING },
   firstname: { type: DataTypes.STRING },
-  secondName: { type: DataTypes.STRING },
-  phoneNumber: { type: DataTypes.STRING, unique: true },
-  dateOfRegistration: { type: DataTypes.DATE },
-  adCount: { type: DataTypes.INTEGER, defaultValue: 0 },
-  rating: { type: DataTypes.DOUBLE, defaultValue: 0 },
-  role: { type: DataTypes.STRING, defaultValue: 'user' },
-});
+  secondname: { type: DataTypes.STRING },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  phonenumber: { type: DataTypes.STRING, unique: true },
+  dateofregistration: { type: DataTypes.DATE},
+  adcount: { type: DataTypes.INTEGER },
+  rating: { type: DateTypes.DOUBLE }
+})
+
+const user_Favorites = sequelize.define('user_fav', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  user_id: { type: DataTypes.INTEGER },
+  ad_id: {type: DataTypes.INTEGER}
+})
+const user_Ads = sequelize.define('user_ads', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  user_id: { type: DataTypes.INTEGER },
+  ad_id: { type: DataTypes.INTEGER }
+})
+const Ad = sequelize.define('ad', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  info_id: { type: DataTypes.INTEGER },
+  rating: { type: DataTypes.DOUBLE },
+  price: { type: DataTypes.INTEGER },
+  img: {type:DataTypes.BLOB}
+})
+const ad_Rating = sequelize.define('ad_rat', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  ad_id: { type: DataTypes.INTEGER },
+  user_id: { type: DataTypes.INTEGER },
+  rating: { type: DataTypes.DOUBLE }
+})
+const ad_Info = sequelize.define('ad_info', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING },
+  description: { type: DataTypes.STRING }
+})
+
+Ad.hasOne(ad_Info)
+Ad.belongsTo(Ad)
+
+User.hasMany(user_Favorites)
+user_Favorites.belongsTo(User)
+
+Ad.hasMany(user_Ads)
+user_Ads.belongsTo(Ad)
+
+User.hasMany(user_Ads)
+user_Ads.belongsTo(User)
+
+Ad.hasMany(user_Favorites)
+user_Favorites.belongsTo(Ad)
+
+User.hasMany(ad_Rating)
+ad_Rating.belongsTo(User)
+
+Ad.hasMany(ad_Rating)
+ad_Rating.belongsTo(Ad)
+
 
 module.exports = {
   User,
-};
+  user_Favorites,
+  user_Ads,
+  Ad,
+  ad_Rating,
+  ad_Info
+}
+
